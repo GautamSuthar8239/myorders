3e5c79898b18a7579aafe3fdf17e3a6fdb1a3b02
Gautam <gsplagiya.8239@gmail.com>
2024-08-16 19:45:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const inc = () => setCount((count) => count + 1);
+  const dec = () => setCount((count) => count - 1);
+  return (
+    <>
+      <div>
+        <h1>Counter App</h1>
+        <p>Current count: {count}</p>
+        <div style={{ display: "flex", gap: "10px", margin: "10px" }}>
+          <button onClick={dec}>-</button>
+          <button onClick={inc}>+</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
